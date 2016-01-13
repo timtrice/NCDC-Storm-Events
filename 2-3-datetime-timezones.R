5121c5b8ac0b79361a9aacb45c95dfe6fcba9c18
timtrice <tim.trice@gmail.com>
2016-01-12 18:05:19 -0600

    chngd names of some vars per 2-1-datetime-add-vars.R

--- 2-3-datetime-timezones.R
+++ 2-3-datetime-timezones.R
@@ -1,67 +1,66 @@
 # Start assigning timezones by CZ_TIMEZONE
 details <- details[CZ_TIMEZONE == "AKST", 
-             BEGIN_YMDHMS := force_tz(BEGIN_DATETIME,
-                                      tz = "America/Anchorage")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX,
+                                              tz = "America/Anchorage")]
 details <- details[CZ_TIMEZONE == "AKST", 
-             END_YMDHMS := force_tz(END_DATETIME, 
-                                    tz = "America/Anchorage")]
+                   END_YMDHMS_L := force_tz(EDT_POSIX, 
+                                            tz = "America/Anchorage")]
 
 details <- details[CZ_TIMEZONE == "AST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "America/Halifax")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "America/Halifax")]
 details <- details[CZ_TIMEZONE == "AST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "America/Halifax")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "America/Halifax")]
 
 details <- details[CZ_TIMEZONE == "CST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "America/Chicago")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "America/Chicago")]
 details <- details[CZ_TIMEZONE == "CST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "America/Chicago")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "America/Chicago")]
 
 details <- details[CZ_TIMEZONE == "ChST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "Pacific/Guam")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "Pacific/Guam")]
 details <- details[CZ_TIMEZONE == "ChST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "Pacific/Guam")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "Pacific/Guam")]
 
 details <- details[CZ_TIMEZONE == "EST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "America/New_York")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "America/New_York")]
 details <- details[CZ_TIMEZONE == "EST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "America/New_York")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "America/New_York")]
 
 details <- details[CZ_TIMEZONE == "HST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "Pacific/Honolulu")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "Pacific/Honolulu")]
 details <- details[CZ_TIMEZONE == "HST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "Pacific/Honolulu")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "Pacific/Honolulu")]
 
 details <- details[CZ_TIMEZONE == "MST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "America/Denver")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "America/Denver")]
 details <- details[CZ_TIMEZONE == "MST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "America/Denver")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "America/Denver")]
 
 details <- details[CZ_TIMEZONE == "PST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "America/Los_Angeles")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "America/Los_Angeles")]
 details <- details[CZ_TIMEZONE == "PST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "America/Los_Angeles")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "America/Los_Angeles")]
 
 details <- details[CZ_TIMEZONE == "SST", 
-                   BEGIN_YMDHMS := force_tz(BEGIN_DATETIME, 
-                                            tz = "Pacific/Samoa")]
+                   BEGIN_YMDHMS_L := force_tz(BDT_POSIX, 
+                                              tz = "Pacific/Samoa")]
 details <- details[CZ_TIMEZONE == "SST", 
-                   END_YMDHMS:= with_tz(END_DATETIME, 
-                                        tz = "Pacific/Samoa")]
+                   END_YMDHMS_L:= force_tz(EDT_POSIX, 
+                                          tz = "Pacific/Samoa")]
 
-details <- details[, BEGIN_YMDHMS_UTC := with_tz(BEGIN_YMDHMS, tz = "UTC")]
-details <- details[, END_YMDHMS_UTC := with_tz(END_YMDHMS, tz = "UTC")]
-
+details <- details[, BEGIN_YMDHMS_UTC := with_tz(BEGIN_YMDHMS_L, tz = "UTC")]
+details <- details[, END_YMDHMS_UTC := with_tz(END_YMDHMS_L, tz = "UTC")]
