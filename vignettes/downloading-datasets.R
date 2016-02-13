c158f847ec68e517bb3f21b92e9066d0acdb8280
timtrice <tim.trice@gmail.com>
2016-02-13 11:00:22 -0600

    adding vignettes

--- vignettes/downloading-datasets.R
+++ vignettes/downloading-datasets.R
@@ -1,3 +1,40 @@
-## ------------------------------------------------------------------------
+## ---- echo = FALSE, message = FALSE--------------------------------------
+knitr::opts_chunk$set(collapse = TRUE, 
+                      comment = "#>", 
+                      fig.width = 7)
 library(NCDCStormEvents)
+
+## ------------------------------------------------------------------------
+ds_list <- get_listings()
+
+## ---- fig.height = 5, fig.cap = "Details, Fatalities and Locations by Year, Size"----
+library(ggplot2)
+
+base_plot <- list( 
+    geom_bar(stat = "identity", position = position_dodge()), 
+    facet_grid(Type ~ ., scales = "free_y"), 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5)), 
+    scale_fill_discrete(name = "Type"), 
+    xlab("Year"))
+
+ggplot(ds_list, aes(x = factor(Year), y = Size, fill = factor(Type))) + 
+    scale_y_continuous(label = function(x){x * 10^-6}) + 
+    ggtitle("Details, Fatalities and Locations by Year, Size") + 
+    ylab("Size (MB)") + 
+    base_plot
+
+## ---- fig.height = 5, fig.cap = "Details, Fatalities and Locations by Year (1950-1995), Size"----
+tmp <- ds_list[Year <= 1995,]
+
+ggplot(tmp, aes(x = factor(Year), y = Size, fill = factor(Type))) + 
+    scale_y_continuous(label = function(x){x * 10^-3}) + 
+    ggtitle("Details, Fatalities and Locations by Year (1950-1995), Size") + 
+    ylab("Size (KB)") + 
+    base_plot
+
+## ------------------------------------------------------------------------
+year <- 1950
+type <- "details"
+
+x <- ds_list[Year == year & Type == type, Size]
 
