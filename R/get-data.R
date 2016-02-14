8a365f3db21a2f2fd044ae68e9a11964f219028c
timtrice <tim.trice@gmail.com>
2016-02-14 17:09:31 -0600

    formatting changes, added get_data, get_datasets

--- R/get-data.R
+++ R/get-data.R
@@ -1,9 +1,17 @@
+ds_url <- function() {
+    "http://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles"
+}
+
+types_available <- function() {
+    c("details", "fatalities", "locations")
+}
+
 #' Get listing of files from NCDC Storm Events
 #'
 #' Get listing of all datasets off the NCDC Storm Events website. 
 #' 
-#' The function returns a data table showing all StormEvents files listed on
-#'      the NCDC server. 
+#' The function returns a data table showing all StormEvents files listed 
+#'      on the NCDC server. 
 #' 
 #' The data table contains the following variables:  
 #' 
@@ -19,13 +27,14 @@
 #' 
 #' This function was created because many datasets listed are empty 
 #'      (typically those listed as 147b file size). And a large majority of 
-#'      them are very small but do have content. However, in the more recent 
-#'      years some of the datasets grow in size exponentially.
+#'      them are very small but do have content. However, in the more 
+#'      recent years some of the datasets grow in size exponentially.
 #' 
 #' So this data table helps give insight into gathering data without 
-#'      overloading on resources. For example, you may want ten years of all 
-#'      data. And, in the early years you can do this very quickly. However, 
-#'      doing it in the 2000's will take a considerable amount of time 
+#'      overloading on resources. For example, you may want ten years of 
+#'      all data. And, in the early years you can do this very quickly. 
+#'      However, doing it in the 2000's will take a considerable amount of 
+#'      time 
 #'      depending on your resources.
 #' 
 #' The listings of datasets can be found at 
@@ -85,9 +94,9 @@ get_listings <- function() {
 
 #' Download NCDC Storm Events datasets
 #' 
-#' Loads all requested datasets. Strongly recommended to use get_listings() 
-#'      first to see what data is available and the file size of the datasets 
-#'      beforeDownloading Datasets
+#' Loads all requested datasets. Strongly recommended to use 
+#'      get_listings() first to see what data is available and the file 
+#'      size of the datasets before downloading datasets
 #'
 #' @param year numeric, vector
 #' @param type character, vector
@@ -103,30 +112,89 @@ get_listings <- function() {
 #' get_data(year = c(1990:1995), type = c("details", "fatalities"))
 #' }
 #' 
-get_data <- function(year = NULL, 
-                     type = c("details", "fatalities", "locations")) {
+get_data <- function(year = NULL, type = types_available()) {
     year <- check_year(year)
     type <- check_type(type)
+
+    summary <- get_listings()
+    
+    requested <- summary[Type %in% type & Year %in% year]
+
+    if("details" %in% type) {
+        dataset <- get_datasets(requested[Type == "details", Name])
+        if(nrow(dataset) > 0) {
+            setkey(dataset, EVENT_ID)
+        } else {
+            warning("No details returned.")
+        }
+    }
+    if("fatalities" %in% type){
+        ds_fatalities <- get_datasets(requested[Type == "fatalities", Name])
+        if(nrow(ds_fatalities) > 0) {
+            setkey(ds_fatalities, EVENT_ID)
+            if(exists("dataset")) {
+                dataset <- ds_fatalities[dataset]
+            } else {
+                dataset <- ds_fatalities
+            }
+        } else {
+            warning("No fatalities returned (Yay!).")
+        }
+    }
+    if("locations" %in% type) {
+        ds_locations <- get_datasets(requested[Type == "locations", Name])
+        if(nrow(ds_locations) > 0) {
+            setkey(ds_locations, EVENT_ID)
+            if(exists("ds_details")) {
+                dataset <- ds_locations[dataset]
+            } else if(exists("ds_fatalities")) {
+                dataset <- ds_locations[dataset]
+            } else {
+                dataset <- ds_locations
+            }
+        } else {
+            warning("No locations returned.")
+        }
+    }
+    
+    dataset
+    
+}
+
+#' Get datasets
+#'
+#' @param dt_names list of Name from get_listings
+#'
+#' @return a data table of all years requested by Type
+#' @export
+get_datasets <- function(dt_names) {
+    dt_names <- paste(ds_url(), dt_names, sep = "/")
+    dt_list <- lapply(dt_names, readr::read_csv)
+    dt <- rbindlist(dt_list)
+    dt
 }
 
 #' Check if Year is numeric input
 #' @param numeric, vector
-#' 
-check_year <- function(year) {
+#' @return year
+check_year <- function(year = NULL) {
     if(!is.numeric(year)) stop("Please provide year(s).")
+    
     year
 }
 
 #' Check if Type is valid character vector
 #' @param type character, vector
-#' 
-check_type <- function(type) {
+#' @return type
+check_type <- function(type = NULL) {
+    if(is.null(type) | !is.character(type)) 
+        stop("Please provide a type.")
     type <- tolower(type)
-    types_avail <- c("details", "fatalities", "locations")
-    if(!is.character(type)) stop("Please provide a type")
-    if(!all(type %in% types_avail))
+    if(!all(type %in% types_available()))
         stop(paste("Invalid type.",
-                   "Please select c(\"details\", \"fatalities\", \"locations\").",
+                   "Please select any(details, fatalities, locations).",
                    sep = " "))
+    
     type
 }
+
