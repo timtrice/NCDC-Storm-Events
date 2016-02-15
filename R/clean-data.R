ed6a8c816a4f2ee51a23e022ee6ad830d174282a
timtrice <tim.trice@gmail.com>
2016-02-14 18:15:26 -0600

    base clean_datetime(), clean_timezones

--- R/clean-data.R
+++ R/clean-data.R
@@ -1,1 +1,20 @@
 library(tidyr)
+library(data.table)
+library(lubridate)
+
+clean_datetime <- function(DT = NULL) {
+    if(!is.data.table(DT)) stop("No data table.")
+    
+    DT <- get_data(2000:2001, "details")
+    DT <- DT[, BeginDateTime := ymd_hms(paste(paste(YEAR, MONTH_NAME, BEGIN_DAY, sep = "-"), 
+                                              sub("[0-9A-Z-]+ ([0-9:]+)", "\\1", DT$BEGIN_DATE_TIME), 
+                                              sep = " "))]
+    DT <- DT[, EndDateTime := ymd_hms(paste(paste(YEAR, MONTH_NAME, END_DAY, sep = "-"), 
+                                              sub("[0-9A-Z-]+ ([0-9:]+)", "\\1", DT$END_DATE_TIME), 
+                                              sep = " "))]
+}
+
+clean_timezones <- function(timezones = NULL) {
+    if(!is.list(timezones)) stop("No timezones.")
+    
+}
