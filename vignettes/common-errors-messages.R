c854ae60ddd9c327523987756ff5c85d72b4a709
timtrice <tim.trice@gmail.com>
2016-11-02 08:59:29 -0500

    vignette modifications

--- vignettes/common-errors-messages.R
+++
@@ -1,6 +0,0 @@
-## ---- echo = FALSE, message = FALSE--------------------------------------
-knitr::opts_chunk$set(collapse = TRUE, 
-                      comment = "#>", 
-                      fig.width = 7)
-library(NCDCStormEvents)
-
