302257f1e88207c95490d89540329a8644990e8d
timtrice <tim.trice@gmail.com>
2016-11-04 10:21:04 -0500

    build fatality_locations dataframe

---
+++ R/dataframes.R
@@ -0,0 +1,22 @@
+#' Initialize dataframes
+
+#' @title fatality_locations
+#' @description Dataframe to hold valid fatalities$location per p.8 of 
+#'   \href{ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/Storm-Data-Export-Format.docx}{Storm Data Export Format document}
+#' @return dataframe
+fatality_locations <- function() {
+  df <- data.frame("fatality_location_id" = c("BF", "BO", "BU", "CA", "CH", 
+                                              "EQ", "GF", "IW", "LS", "MH", 
+                                              "OT", "OU", "PH", "PS", "SC", 
+                                              "TE", "UT", "VE"), 
+                   "FATALITY_LOCATION" = c("Ball Field", "Boating", "Business", 
+                                  "Camping", "Church", 
+                                  "Heavy Equip/Construction", "Golfing", 
+                                  "In Water", "Long Span Roof", 
+                                  "Mobile/Trailer Home", "Other/Unknown", 
+                                  "Outside/Open Areas", "Permanent Home", 
+                                  "Permanent Structure", "School", "Telephone", 
+                                  "Under Tree", "Vehicle and/or Towed Trailer"), 
+                   stringsAsFactors = FALSE)
+  return(df)  
+}
