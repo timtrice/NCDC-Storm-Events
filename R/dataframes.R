b260753c312f91e48173381c243c48eb99fd5e98
timtrice <tim.trice@gmail.com>
2016-11-04 12:33:35 -0500

    add skeleton dataframe for states

--- R/dataframes.R
+++ R/dataframes.R
@@ -25,3 +25,29 @@ fatality_locations <- function() {
                    stringsAsFactors = FALSE)
   return(df)  
 }
+
+#' @title state_fips
+#' @description Create dataframe to hold STATE and corresponding state_id and 
+#'   FIPS codes.
+#' @return dataframe
+state_fips <- function() {
+  df <- data.frame(
+    "state_id" = c(1:68), 
+    "STATE" = c("ALABAMA", "ALASKA", "AMERICAN SAMOA", "ARIZONA", "ARKANSAS", 
+                "ATLANTIC NORTH", "ATLANTIC SOUTH", "CALIFORNIA", "COLORADO", 
+                "CONNECTICUT", "DELAWARE", "DISTRICT OF COLUMBIA", "E PACIFIC", 
+                "FLORIDA", "GEORGIA", "GUAM", "GULF OF ALASKA", 
+                "GULF OF MEXICO", "HAWAII", "HAWAII WATERS", "IDAHO", 
+                "ILLINOIS", "INDIANA", "IOWA", "KANSAS", "KENTUCKY", 
+                "LAKE ERIE", "LAKE HURON", "LAKE MICHIGAN", "LAKE ONTARIO", 
+                "LAKE ST CLAIR", "LAKE SUPERIOR", "LOUISIANA", "MAINE", 
+                "MARYLAND", "MASSACHUSETTS", "MICHIGAN", "MINNESOTA", 
+                "MISSISSIPPI", "MISSOURI", "MONTANA", "NEBRASKA", "NEVADA", 
+                "NEW HAMPSHIRE", "NEW JERSEY", "NEW MEXICO", "NEW YORK", 
+                "NORTH CAROLINA", "NORTH DAKOTA", "OHIO", "OKLAHOMA", "OREGON",
+                "PENNSYLVANIA", "PUERTO RICO", "RHODE ISLAND", "SOUTH CAROLINA", 
+                "SOUTH DAKOTA", "ST LAWRENCE R", "TENNESSEE", "TEXAS", "UTAH", 
+                "VERMONT", "VIRGINIA", "VIRGIN ISLANDS", "WASHINGTON", 
+                "WEST VIRGINIA", "WISCONSIN", "WYOMING"), 
+    "FIPS" = c(1:68))
+}
