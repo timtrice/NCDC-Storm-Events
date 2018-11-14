a315b426f7f3bf961704cbbd62941e89192e9335
Tim Trice <tim.trice@shell.com>
2018-11-14 14:14:54 -0600

    Add raw data, script to download

---
+++ code/01_get_data.R
@@ -0,0 +1,61 @@
+#' @author Tim Trice <tim.trice@gmail.com>
+#' @description Import NCDC Storm Events Database
+#' @source https://www.ncdc.noaa.gov/stormevents/
+
+# ---- libraries ----
+library(curl)
+library(dplyr)
+library(glue)
+library(here)
+library(purrr)
+library(readr)
+
+# ---- sources ----
+source(here("./code/functions.R"))
+
+# ---- settings ----
+#' FTP URL
+ftp <- "ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/"
+
+#' Three datasets to download and their expected width
+tables <- list(
+  "details" = 51L,
+  "fatalities" = 11L,
+  "locations" = 11L
+)
+
+# ---- connection ----
+#' Establish connection, get list of gz datasets
+con = curl(ftp, "r")
+tbl = read.table(con, stringsAsFactors = TRUE, fill = TRUE)
+close(con)
+
+# ---- by_table ----
+#' Split out the datasets into their own lists. Will end up with a list of
+#' length 3 for each table containing all related dataset URLs
+by_table <-
+  map(
+    .x = glue("^StormEvents_{names(tables)}"),
+    .f = grep,
+    x = tbl$V9,
+    value = TRUE
+  ) %>%
+  set_names(nm = names(tables))
+
+# ---- load-data ----
+df <-
+  map2(
+    .x = names(tables),
+    .y = flatten_int(tables),
+    .f = import_datasets
+  ) %>%
+  set_names(nm = names(tables))
+
+#' Bring `EVENT_ID` and table's respective ID field to front
+df$fatalities <- select(df$fatalities, EVENT_ID, FATALITY_ID, everything())
+df$locations <- select(df$locations, EPISODE_ID, EVENT_ID, everything())
+
+# ---- save-data ----
+write_csv(df$details, here("./data/details.csv"))
+write_csv(df$fatalities, here("./data/fatalities.csv"))
+write_csv(df$locations, here("./data/locations.csv"))
