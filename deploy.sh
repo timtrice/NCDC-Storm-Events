d323bdd9a68359ebb16d1f9dba14d8c9a978399e
Tim Trice <tim.trice@gmail.com>
2019-07-05 07:53:30 -0500

    Rewrite deploy for this repo

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,10 @@ git config --global user.email "tim.trice@gmail.com"
 git clone https://github.com/timtrice/ncdc_storm_events.git
 cd ncdc_storm_events
 git checkout $TRAVIS_BRANCH
-Rscript --verbose R/01_install_packages.R
-rm -r docs
-Rscript -e 'rmarkdown::render_site(".");'
-
-if [ ! -d "docs" ]
-then
-  echo "Docs directory does not exist"
-  exit 1
-fi
-
+Rscript -e 'remotes::install_deps();'
+Rscript --verbose code/02_get_data.R
+Rscript -e 'workflowr::build()'
 git add --force docs
-MSG="Rebuild website, (Travis Build #$TRAVIS_BUILD_NUMBER) [skip ci]"
+MSG="Rebuild website #$TRAVIS_BUILD_NUMBER [skip ci]"
 git commit -m "$MSG"
 git push --force $FULL_REPO $TRAVIS_BRANCH
-Rscript -e 'remotes::install_github("karthik/holepunch");'
-Rscript -e 'holepunch::build_binder();'
