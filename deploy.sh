70ae9393474e38f1fb2bb4caf57c50910d507878
Tim Trice <tim.trice@gmail.com>
2019-07-05 09:04:32 -0500

    Do not install dependencies; already done

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ git config --global user.name "Travis CI"
 git config --global user.email "tim.trice@gmail.com"
 git clone https://github.com/timtrice/ncdc_storm_events.git --branch $TRAVIS_BRANCH --single-branch
 cd ncdc_storm_events
-git checkout $TRAVIS_BRANCH
-Rscript -e 'remotes::install_deps();'
 Rscript --verbose code/02_get_data.R
 Rscript -e 'workflowr::build()'
 git add --force docs
