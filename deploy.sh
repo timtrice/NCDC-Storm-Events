f43bdafbf92ec0be0ee45c645e4c34db949569ca
Tim Trice <tim.trice@gmail.com>
2019-07-05 08:57:34 -0500

    git clone only $TRAVIS_BRANCH
    [Build
    throws error trying to clone master branch due to git lfs. As these
    files will no longer be used I've modified deploy to only clone the
    testing branch.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ GH_REPO="@github.com/$TRAVIS_REPO_SLUG.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 git config --global user.name "Travis CI"
 git config --global user.email "tim.trice@gmail.com"
-git clone https://github.com/timtrice/ncdc_storm_events.git
+git clone https://github.com/timtrice/ncdc_storm_events.git --branch $TRAVIS_BRANCH --single-branch
 cd ncdc_storm_events
 git checkout $TRAVIS_BRANCH
 Rscript -e 'remotes::install_deps();'
